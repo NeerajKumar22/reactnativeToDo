4a84f32c765144fc253e44cedc2a48376e6f63b7
NeerajKumar22 <ineerajkr@gmail.com>
2020-05-07 15:23:24 +0530

    manage indentaion

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import Header from "./components/header";
 import TodoItem from "./components/todoitem";
 
 export default function App() {
-	const [todos, setTodos] = useState([
-		{text: "buy coffee", key: "1"},
-		{text: "create an app", key: "2"},
-		{text: "play on the switch", key: "3"}
-	])
+  const [todos, setTodos] = useState([
+    {text: "buy coffee", key: "1"},
+    {text: "create an app", key: "2"},
+    {text: "play on the switch", key: "3"}
+  ])
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key);
@@ -17,19 +17,19 @@ export default function App() {
   }
   return (
     <View style={styles.container}>
-			<Header />
-			<View style={styles.content}>
-				{/* to form */}
-				<View style={styles.list}>
-					<FlatList
-						data={todos}
-						renderItem={({item}) => (
-                <TodoItem item={item} pressHandler={pressHandler}/>
-              )
-						}
-					/>
-				</View>
-			</View>
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({item}) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )
+            }
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -39,11 +39,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-	content: {
-		padding: 40,
-	},
-	list: {
-		marginTop: 20,
-	}
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
 
--- components/header.js
+++ components/header.js
@@ -2,24 +2,24 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Header(){
-	return(
-		<View style={styles.header}>
-			<Text style={styles.title}>My ToDo</Text>
-		</View>
-	)
+  return(
+    <View style={styles.header}>
+      <Text style={styles.title}>My ToDo</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	header: {
-		height: 80,
-		paddingTop: 38,
-		backgroundColor: "red"
-	},
-	title: {
-		textAlign: "center",
-		color: "#fff",
-		fontSize: 20,
-		fontWeight: "bold",
-	}
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "red"
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  }
 })
 
